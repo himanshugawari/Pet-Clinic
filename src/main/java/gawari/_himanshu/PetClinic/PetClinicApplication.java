b3f8d3f3480fc4f229af9b55ef9cd733f6f469cc
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-10-13 18:14:13 +0530

    Initial Base Code for Spring Boot Pet Clinic

---
+++ src/main/java/gawari/_himanshu/PetClinic/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package gawari._himanshu.PetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
