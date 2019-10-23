f0ad4a5f8647a4048c60e6325ab3e7884ca28731
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-10-23 15:32:54 +0530

    Added list list to vet index page . closes #15

--- Pet-Clinic-Web/src/main/java/gawari/_himanshu/PetClinic/controllers/VetController.java
+++ Pet-Clinic-Web/src/main/java/gawari/_himanshu/PetClinic/controllers/VetController.java
@@ -1,13 +1,26 @@
 package gawari._himanshu.PetClinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import gawari._himanshu.PetClinic.services.VetService;
 
 @Controller
 public class VetController {
 
+	private final VetService vetService;
+
+	@Autowired
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({ "/vets", "/vets/index", "/vet_index", "/vet_index.html" })
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/vet_index";
 	}
 }
