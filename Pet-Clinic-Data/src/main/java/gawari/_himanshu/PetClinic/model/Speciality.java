ddd43c0f1856f977196b3fc40585c183c16f3331
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-10-30 21:50:51 +0530

    added speciality entity and associate to vet. closes #30

--- Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/model/Speciality.java
+++ Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/model/Speciality.java
@@ -1,6 +1,6 @@
 package gawari._himanshu.PetClinic.model;
 
-public class Speciality {
+public class Speciality extends BaseEntity {
 	private String description;
 
 	public String getDescription() {
