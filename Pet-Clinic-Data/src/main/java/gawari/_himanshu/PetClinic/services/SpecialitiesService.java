4bb4c0d94292ca377aae937d3527773ef52fed8d
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-11-01 10:27:14 +0530

    Added Specialties impl. closes #34

---
+++ Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package gawari._himanshu.PetClinic.services;
+
+import gawari._himanshu.PetClinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
---
+++ Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,43 @@
+package gawari._himanshu.PetClinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import gawari._himanshu.PetClinic.model.Speciality;
+import gawari._himanshu.PetClinic.services.SpecialitiesService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+	@Override
+	public Set<Speciality> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		// TODO Auto-generated method stub
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+}
