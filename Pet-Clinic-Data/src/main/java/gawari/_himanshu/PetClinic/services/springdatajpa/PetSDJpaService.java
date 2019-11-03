af27de6a54d8cec4ed6f7ceea52e1b43faca89c6
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-11-03 15:24:19 +0530

    addedspring data jpa pet service closes #46

---
+++ Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,53 @@
+package gawari._himanshu.PetClinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import gawari._himanshu.PetClinic.model.Pet;
+import gawari._himanshu.PetClinic.repositories.PetRepository;
+import gawari._himanshu.PetClinic.services.PetService;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+	private final PetRepository petRepository;
+
+	@Autowired
+	public PetSDJpaService(PetRepository petRepository) {
+		super();
+		this.petRepository = petRepository;
+	}
+
+	@Override
+	public Set<Pet> findAll() {
+		Set<Pet> pets = new HashSet<>();
+		petRepository.findAll().forEach(pets::add);
+		return pets;
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		return petRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		return petRepository.save(object);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		petRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		petRepository.deleteById(id);
+	}
+
+}
