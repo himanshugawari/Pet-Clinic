37e5a9163f1bb626d331794dda3b0955c27a33df
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-11-03 13:52:59 +0530

    updated owner to jpa mappings closes #39

--- Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/model/Person.java
+++ Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/model/Person.java
@@ -1,7 +1,15 @@
 package gawari._himanshu.PetClinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@SuppressWarnings("serial")
+@MappedSuperclass
 public class Person extends BaseEntity {
+	@Column(name = "first_name")
 	private String firstName;
+
+	@Column(name = "last_name")
 	private String lastName;
 
 	public String getFirstName() {
