0120387cc6b68184bfca0f48add9b02f6332a61c
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-11-03 12:50:59 +0530

    base entity implemented closes #38

--- Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/model/BaseEntity.java
+++ Pet-Clinic-Data/src/main/java/gawari/_himanshu/PetClinic/model/BaseEntity.java
@@ -2,7 +2,16 @@ package gawari._himanshu.PetClinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@SuppressWarnings("serial")
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
