c9b38d7e0d534b57a293f925f245b5e604c257dc
Himanshu Gawari <himanshugawarigit@gmail.com>
2019-11-11 15:13:43 +0530

    upgraded to junit5 closes #4

--- Pet-Clinic-Web/src/test/java/gawari/_himanshu/PetClinic/PetClinicApplicationTests.java
+++ Pet-Clinic-Web/src/test/java/gawari/_himanshu/PetClinic/PetClinicApplicationTests.java
@@ -1,11 +1,10 @@
 package gawari._himanshu.PetClinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+//@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PetClinicApplicationTests {
 
